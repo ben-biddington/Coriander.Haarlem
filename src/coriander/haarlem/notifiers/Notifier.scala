7097e2abd5c6d18b7a5b48fe447bd6e99f653c45
Ben Biddington <ben.biddington@gmail.com>
2010-01-19 14:08:47 +0000

    Moved Notifier

---
+++ src/coriander/haarlem/notifiers/Notifier.scala
@@ -0,0 +1,53 @@
+package coriander.haarlem.notifiers
+
+
+import jetbrains.buildServer.Build
+import jetbrains.buildServer.notification.Notificator
+import jetbrains.buildServer.responsibility.TestNameResponsibilityEntry
+import jetbrains.buildServer.serverSide.{SProject, SBuildType, SRunningBuild}
+import jetbrains.buildServer.vcs.VcsRoot
+import jetbrains.buildServer.users.SUser
+import java.util.Set
+
+class Notifier extends Notificator {
+	def getDisplayName = "xxx"
+	def getNotificatorType = "xxx"
+
+	def notifyBuildStarted(runningBuild : SRunningBuild, users : Set[SUser]) {
+		val theBuildPath = runningBuild.getCurrentPath;
+		val percentComplete = runningBuild.getCompletedPercent;
+	}
+
+    def notifyBuildSuccessful(runningBuild : SRunningBuild, users : Set[SUser]) {}
+
+    def notifyBuildFailed(runningBuild : SRunningBuild, users : Set[SUser]) {}
+
+    def notifyLabelingFailed(
+		build : Build,
+		vcsRoot : VcsRoot,
+		throwable : java.lang.Throwable,
+		users : Set[SUser]
+	) {}
+
+    def notifyBuildFailing(runningBuild : SRunningBuild, users : Set[SUser]) { }
+
+    def notifyBuildProbablyHanging(runningBuild : SRunningBuild, users : Set[SUser]) { }
+
+    def notifyResponsibleChanged(buildType : SBuildType, users : Set[SUser]) {}
+
+    def notifyResponsibleAssigned(buildType : SBuildType, users : Set[SUser]) {}
+
+    def notifyResponsibleChanged(
+		testNameResponsibilityEntry : TestNameResponsibilityEntry,
+		testNameResponsibilityEntry1 : TestNameResponsibilityEntry,
+		project : SProject,
+		users : Set[SUser]
+	) { }
+
+    def notifyResponsibleAssigned(
+		testNameResponsibilityEntry : TestNameResponsibilityEntry,
+		testNameResponsibilityEntry1 : TestNameResponsibilityEntry,
+		project : SProject,
+		users : Set[SUser]
+	) { }
+}
---
+++ test/coriander/haarlem/unit/tests/notifiers/NotifierTests.scala
@@ -0,0 +1,34 @@
+package coriander.haarlem.unit.tests.notifiers
+
+import org.junit.Test
+import org.junit.Assert._
+import org.hamcrest.core.Is._
+import org.hamcrest.core.IsNot._
+import org.hamcrest.core.IsEqual._
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+import jetbrains.buildServer.serverSide.SRunningBuild
+import jetbrains.buildServer.users.SUser
+import coriander.haarlem.notifiers.{Notifier}
+import java.util.HashSet
+
+class NotifierTests {
+    @Test
+	def notifyBuildStarted_reads_the_current_build_path {
+		given_any_running_build
+		
+		val exampleUsers = new HashSet[SUser]
+
+ 		new Notifier().notifyBuildStarted(mockRunningBuild, exampleUsers)
+
+		verify(mockRunningBuild, times(1)).getCurrentPath()
+    }
+
+	private def given_any_running_build {
+		mockRunningBuild = mock(classOf[SRunningBuild])
+		when(mockRunningBuild.getCurrentPath()).
+		thenReturn("xxx");
+	}
+
+	var mockRunningBuild : SRunningBuild = null
+}
