903773374058d8ff20e6935b5df51ab39e799a93
Ben Biddington <ben.biddington@gmail.com>
2010-01-19 12:49:36 +0000

    Refactor: tify

--- src/coriander/haarlem/Notifier.scala
+++ src/coriander/haarlem/Notifier.scala
@@ -5,65 +5,45 @@ import jetbrains.buildServer.notification.Notificator
 import jetbrains.buildServer.responsibility.TestNameResponsibilityEntry
 import jetbrains.buildServer.serverSide.{SProject, SBuildType, SRunningBuild}
 import jetbrains.buildServer.vcs.VcsRoot
+import jetbrains.buildServer.users.SUser
 
 class Notifier extends Notificator {
 	def getDisplayName = "xxx"
 
 	def getNotificatorType = "xxx"
 
-	def notifyBuildStarted(
-		sRunningBuild : SRunningBuild ,
-		sUsers : java.util.Set[jetbrains.buildServer.users.SUser]
-	) { }
+	def notifyBuildStarted(runningBuild : SRunningBuild, users : Set[SUser]) { }
 
-    def notifyBuildSuccessful(
-		sRunningBuild : SRunningBuild, 
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
-	) {}
+    def notifyBuildSuccessful(runningBuild : SRunningBuild, users : Set[SUser]) {}
 
-    def notifyBuildFailed(
-		sRunningBuild : SRunningBuild,
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
-	) {}
+    def notifyBuildFailed(runningBuild : SRunningBuild, users : Set[SUser]) {}
 
     def notifyLabelingFailed(
 		build : Build,
 		vcsRoot : VcsRoot,
 		throwable : java.lang.Throwable,
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
+		users : Set[SUser]
 	) {}
 
-    def notifyBuildFailing(
-		sRunningBuild : SRunningBuild ,
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
-	) { }
+    def notifyBuildFailing(runningBuild : SRunningBuild, users : Set[SUser]) { }
 
-    def notifyBuildProbablyHanging(
-		sRunningBuild : SRunningBuild ,
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
-	) { }
+    def notifyBuildProbablyHanging(runningBuild : SRunningBuild, users : Set[SUser]) { }
 
-    def notifyResponsibleChanged(
-		sBuildType : SBuildType, 
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
-	) {}
+    def notifyResponsibleChanged(buildType : SBuildType, users : Set[SUser]) {}
 
-    def notifyResponsibleAssigned(
-		sBuildType : SBuildType,
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
-	) {}
+    def notifyResponsibleAssigned(buildType : SBuildType, users : Set[SUser]) {}
 
     def notifyResponsibleChanged(
 		testNameResponsibilityEntry : TestNameResponsibilityEntry,
 		testNameResponsibilityEntry1 : TestNameResponsibilityEntry,
-		sProject : SProject,
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
+		project : SProject,
+		users : Set[SUser]
 	) { }
 
     def notifyResponsibleAssigned(
 		testNameResponsibilityEntry : TestNameResponsibilityEntry ,
 		testNameResponsibilityEntry1 : TestNameResponsibilityEntry ,
-		sProject : SProject,
-		users : java.util.Set[jetbrains.buildServer.users.SUser]
+		project : SProject,
+		users : Set[SUser]
 	) {}
 }
