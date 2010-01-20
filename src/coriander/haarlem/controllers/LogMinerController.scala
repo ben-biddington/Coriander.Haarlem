3f74812c74f3d094d39a6de10162c3d6e439cead
Ben Biddington <ben.biddington@gmail.com>
2010-01-20 13:32:04 +0000

    In order to create a controller for teamcity, a couple of libraries are required (Spring, jswdk)

---
+++ src/coriander/haarlem/controllers/LogMinerController.scala
@@ -0,0 +1,11 @@
+package coriander.haarlem.controllers
+
+import jetbrains.buildServer.controllers.BaseController
+import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+
+class LogMinerController extends BaseController {
+	def doHandle(request : HttpServletRequest, response : HttpServletResponse) : ModelAndView = {
+		 throw new Exception("Not implemented yet")     
+	}
+}
