99878a093bd294ac85e35c99d39bf69bfc50ef15
Ben Biddington <ben.biddington@gmail.com>
2010-01-20 13:59:59 +0000

    Added super invocation to LogMinerController

--- src/coriander/haarlem/controllers/LogMinerController.scala
+++ src/coriander/haarlem/controllers/LogMinerController.scala
@@ -3,9 +3,12 @@ package coriander.haarlem.controllers
 import jetbrains.buildServer.controllers.BaseController
 import org.springframework.web.servlet.ModelAndView
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import jetbrains.buildServer.serverSide.SBuildServer
 
-class LogMinerController extends BaseController {
-	def doHandle(request : HttpServletRequest, response : HttpServletResponse) : ModelAndView = {
-		 throw new Exception("Not implemented yet")     
+class LogMinerController(buildServer : SBuildServer) extends BaseController(buildServer) {
+	def this() = this(null)
+	
+	def doHandle(request : HttpServletRequest, response : HttpServletResponse) : ModelAndView = {		
+		throw new Exception("Not implemented yet")
 	}
 }
