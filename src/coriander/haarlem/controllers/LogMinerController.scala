ed63676aa263915ab1de36bab5ecb46e18407114
Ben Biddington <ben.biddington@gmail.com>
2010-01-20 17:46:14 +0000

    A bit more impl

--- src/coriander/haarlem/controllers/LogMinerController.scala
+++ src/coriander/haarlem/controllers/LogMinerController.scala
@@ -4,11 +4,27 @@ import jetbrains.buildServer.controllers.BaseController
 import org.springframework.web.servlet.ModelAndView
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import jetbrains.buildServer.serverSide.SBuildServer
+import javax.management.Query
 
 class LogMinerController(buildServer : SBuildServer) extends BaseController(buildServer) {
 	def this() = this(null)
 	
-	def doHandle(request : HttpServletRequest, response : HttpServletResponse) : ModelAndView = {		
-		throw new Exception("Not implemented yet")
+	def doHandle(
+		request : HttpServletRequest,
+		response : HttpServletResponse
+	) : ModelAndView = {
+		require(buildServer != null, "Not much can be done without a build server.")
+
+		val query = request.getQueryString()
+
+		if (false == query.contains("build")) {
+			throw new Exception("Not implemented yet")
+		}
+
+		val buildNumber = buildServer.getBuildNumber
+		
+		response.setStatus(200)
+
+		return null
 	}
 }
