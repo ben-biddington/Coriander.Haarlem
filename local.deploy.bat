9da2bda1a24dd9efc10ab89f2bf50ea3ea3237ed
Ben Biddington <ben.biddington@gmail.com>
2010-01-22 18:31:33 +0000

    Reorganising build files

--- local.deploy.bat
+++ local.deploy.bat
@@ -1,7 +1,9 @@
 @echo off
 
 set web_root="C:\Teamcity\webapps\ROOT\WEB-INF\lib"
+set plugin_root="C:\TeamCity\webapps\ROOT\WEB-INF\plugins"
 
 echo Copying jar: %1 to %web_root%
 
-copy /Y "%1" %web_root%
+copy /Y "%1" %web_root%  REM Teamcity 4.x, shouldn't have to do this
+copy /Y "%1" %plugin_root%
