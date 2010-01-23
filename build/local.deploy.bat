231540591801571d621124b9a64e6e6b146f2e0a
Ben Biddington <ben.biddington@gmail.com>
2010-01-23 13:30:03 +0000

    Updated local deploy

--- build/local.deploy.bat
+++ build/local.deploy.bat
@@ -1,12 +1,18 @@
 @echo off
+set plugin_name=coriander-haarlem
 
 set web_root="C:\Teamcity\webapps\ROOT\WEB-INF\lib"
 set plugin_root="C:\TeamCity\webapps\ROOT\WEB-INF\plugins"
+set plugin_root_1="C:\TeamCity\webapps\ROOT\plugins"
 
-echo Copying jar: %1 to %web_root%
-
+REM echo Copying jar: %1 to %web_root%
 REM Teamcity 4.x, shouldn't have to do this
-copy /Y "%1" %web_root%
+REM copy /Y "%1" %web_root%
 
+REM echo Copying jar: %1 to %plugin_root%
 REM This should be enough
-copy /Y "%1" %plugin_root%
+REM copy /Y "%1" %plugin_root%
+
+mkdir "%plugin_root%\coriander-haarlem\server"
+echo Copying jar: %1 to "%plugin_root%\%plugin_name%\server"
+copy /Y "%1" "%plugin_root%\%plugin_name%\server"
