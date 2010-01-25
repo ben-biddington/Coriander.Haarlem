0214400fcffb784392c84a1a71b4798a5410f524
Ben Biddington <ben.biddington@gmail.com>
2010-01-25 09:43:51 +0000

    Removed extra view-related guff from DilbertController

--- src/coriander/haarlem/controllers/DilbertController.scala
+++ src/coriander/haarlem/controllers/DilbertController.scala
@@ -9,16 +9,7 @@ class DilbertController extends BaseController {
 		httpServletRequest : HttpServletRequest ,
 		httpServletResponse : HttpServletResponse
 	) : ModelAndView = {
-
-		httpServletResponse.setContentType("text/html")
-
-		// TODO: The following fails because the jsp file cannot be located
+		// TODO: Still crap, shouldn't need to resolve view paths myself
 		new ModelAndView("plugins/coriander-haarlem/default.jsp")
-	}
-
-	private def getLatestDilbert = {
-		// TODO: Get latest from: http://feeds.dilbert.com/DilbertDailyStrip?format=xml
-		"http://dilbert.com/dyn/str_strip/000000000/" +
-		"00000000/0000000/000000/70000/9000/500/79578/79578.strip.gif"
 	}
 }
