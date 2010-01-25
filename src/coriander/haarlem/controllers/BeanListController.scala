d66be40e32b2d3b6737d1d18aad14f688d0353f6
Ben Biddington <ben.biddington@gmail.com>
2010-01-25 17:48:59 +0000

    Added BeanListController

---
+++ src/coriander/haarlem/controllers/BeanListController.scala
@@ -0,0 +1,51 @@
+package coriander.haarlem.controllers
+
+import jetbrains.buildServer.controllers.BaseController
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import org.springframework.web.servlet.ModelAndView
+import java.io.{PrintWriter}
+
+class BeanListController extends BaseController {
+	override protected def doHandle(
+		request : HttpServletRequest,
+		response : HttpServletResponse
+	) : ModelAndView = {
+		response.setContentType("text/html")
+
+		response.getWriter.write("<html><head></head><body>")
+
+		printBeans(response.getWriter)
+
+		response.getWriter.write("</body></html>")
+		
+		null
+	}
+
+	private def printBeans(out : PrintWriter) {
+		val allBeans = getApplicationContext().getBeanDefinitionNames.
+			toList.sort((left, right) => left < right)
+
+		out write("<ul>")
+
+		allBeans.foreach(name => {
+			val bean = getApplicationContext().getBean(name)
+			out write(
+				"<li>Bean: " + name + "</li>" +
+				printInterfaces(bean, out)
+			)	
+		});
+
+		out write("</ul>")
+	}
+
+	private def printInterfaces(c : java.lang.Object, out : PrintWriter) {
+    	val interfaces = c.getClass.getInterfaces
+		if (false == interfaces.isEmpty) {
+			out.write("<ul>")
+			interfaces.foreach(interface => out.write("<li>" + interface + "</li>"));
+			out.write("</ul>")
+		}
+	}
+	
+	def register() {}
+}
