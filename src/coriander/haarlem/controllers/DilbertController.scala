7b9f3571433c0124fd0bf3a5861b8aa1f87d9aa8
Ben Biddington <ben.biddington@gmail.com>
2010-01-25 17:20:43 +0000

    Modified bean config

--- src/coriander/haarlem/controllers/DilbertController.scala
+++ src/coriander/haarlem/controllers/DilbertController.scala
@@ -6,10 +6,18 @@ import org.springframework.web.servlet.ModelAndView
 
 class DilbertController extends BaseController {
 	override protected def doHandle(
-		httpServletRequest : HttpServletRequest ,
+		httpServletRequest : HttpServletRequest,
 		httpServletResponse : HttpServletResponse
 	) : ModelAndView = {
 		// TODO: Still crap, shouldn't need to resolve view paths myself
 		new ModelAndView("plugins/coriander-haarlem/default.jsp")
+	}
+
+	def register() {
+		// TODO: Register route here
+		// see: http://www.jetbrains.net/confluence/display/TCD5/Web+UI+Extensions#WebUIExtensions-DevelopPageExtention
+		println("Registering DilbertController")
+
+		logger.info("Registering DilbertController")
 	}
 }
--- src/coriander/haarlem/tabs/DilbertTab.scala
+++ src/coriander/haarlem/tabs/DilbertTab.scala
@@ -1,13 +1,27 @@
 package coriander.haarlem.tabs
 
-import jetbrains.buildServer.web.openapi.{PageExtension, CustomTab}
 import javax.servlet.http.HttpServletRequest
+import org.springframework.context.{ApplicationContext, ApplicationContextAware}
+import jetbrains.buildServer.web.openapi.{PlaceId, PagePlaces, PageExtension, CustomTab}
 
-class DilbertTab extends CustomTab with PageExtension {
-	def getTabId = "coriander.haarlem.dilbert"
-	def getTabTitle = "Dilbert"
-	def getIncludeUrl = "DilbertController"
-	def getPluginName = "dilbert tab"
+class DilbertTab
+	extends CustomTab
+	with PageExtension
+	with ApplicationContextAware {
+
+	def register() {
+		// TODO: Find anj instance of PagePlaces
+		//val bean = applicationContext.getBean("pagePlaces").asInstanceOf[PagePlaces];
+	}
+
+	def setApplicationContext(applicationContext : ApplicationContext) {
+		this.applicationContext = applicationContext
+	}
+	
+	def getTabId 		= "coriander.haarlem.dilbert"
+	def getTabTitle 	= "Dilbert"
+	def getIncludeUrl 	= "DilbertController"
+	def getPluginName 	= "dilbert tab"
 
 	def fillModel(
 		stringObjectMap : java.util.Map[java.lang.String,java.lang.Object],
@@ -23,4 +37,10 @@ class DilbertTab extends CustomTab with PageExtension {
 	def isVisible : Boolean = true
 	def getCssPaths : java.util.List[String] = new java.util.ArrayList[String]()
 	def getJsPaths : java.util.List[String] = new java.util.ArrayList[String]()
+
+	def setPlaceId(placeId : String) { }
+	def setPluginName(pluginName: String) { }
+	def setIncludeUrl(includeUrl: String) { }
+
+	private var applicationContext : ApplicationContext = null
 }
