4cabf0a7a19074123b7a692d93ff544eb558e026
Ben Biddington <ben.biddington@gmail.com>
2010-01-26 08:48:27 +0000

    Simplified deployment: now don't need to deply anywhere but plugins folder

--- build/local.deploy.bat
+++ build/local.deploy.bat
@@ -1,17 +1,8 @@
 @echo off
 set plugin_name=coriander-haarlem
 
-set web_root="C:\Teamcity\webapps\ROOT\WEB-INF\lib"
 set plugin_root="C:\TeamCity\webapps\ROOT\WEB-INF\plugins"
-set plugin_root_1="C:\TeamCity\webapps\ROOT\plugins"
-
-echo Copying jar: %1 to %web_root%
-copy /Y "%1" %web_root%
-
-REM echo Copying jar: %1 to %plugin_root%
-REM This should be enough
-REM copy /Y "%1" %plugin_root%
 
 mkdir "%plugin_root%\coriander-haarlem\server"
-echo Copying jar: %1 to "%plugin_root%\%plugin_name%\server"
+echo Copying "%1" to "%plugin_root%\%plugin_name%\server"
 copy /Y "%1" "%plugin_root%\%plugin_name%\server"
