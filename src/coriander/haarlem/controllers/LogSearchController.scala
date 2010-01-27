40d1a040f0242657ea0ca331a3468fa4390981db
Ben Biddington <ben.biddington@gmail.com>
2010-01-27 14:08:30 +0000

    Added some search views, adjusted local deploy to include all views

--- src/coriander/haarlem/controllers/LogSearchController.scala
+++ src/coriander/haarlem/controllers/LogSearchController.scala
@@ -5,8 +5,10 @@ import org.springframework.web.servlet.ModelAndView
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import jetbrains.buildServer.serverSide.SBuildServer
 import org.coriander.{QueryParser}
+import jetbrains.buildServer.web.openapi.WebControllerManager
 
-class LogSearchController(buildServer : SBuildServer) extends BaseController(buildServer) {
+class LogSearchController(buildServer : SBuildServer)
+	extends BaseController(buildServer) {
 	def this() = this(null)
 	
 	def doHandle(
@@ -14,16 +16,23 @@ class LogSearchController(buildServer : SBuildServer) extends BaseController(bui
 		response : HttpServletResponse
 	) : ModelAndView = {
 		require(buildServer != null, "Not much can be done without a build server.")
+//
+//		val query = new QueryParser().parse(request.getQueryString())
+//
+//		require(query.contains("build"), "Requires build identifier")
+//		require(query.contains("q"), "Requires search term")
+//
+//		val buildNumber = buildServer.getBuildNumber
+//
+//		response.setStatus(200)
 
-		val query = new QueryParser().parse(request.getQueryString())
+		new ModelAndView("plugins/coriander-haarlem/search/default.jsp")
+	}
 
-		require(query.contains("build"), "Requires build identifier")
-		require(query.contains("q"), "Requires search term")
+	def register() {
+		val mgr = getApplicationContext.
+			getBean("webControllerManager", classOf[WebControllerManager])
 
-		val buildNumber = buildServer.getBuildNumber
-		
-		response.setStatus(200)
-
-		return null
+		mgr.registerController("/log-search.html", this)
 	}
 }
---
+++ src/coriander/haarlem/views/search/input.jsp
@@ -0,0 +1,5 @@
+<form action="" method="get">
+    <label for="q" value="Search" />
+    <input type="text" id="q" name="q" />
+    <input type="submit" id="go" name="go" value="Go"/>
+</form>
