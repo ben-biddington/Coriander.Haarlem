479de98d54ca9cbf8bbba9a9fed317cf2f4ae265
Ben Biddington <ben.biddington@gmail.com>
2010-01-31 20:05:33 +0000

    Updated, fixed where error

--- bin/search.bat
+++ bin/search.bat
@@ -2,8 +2,12 @@
 
 set count=100
 set flags=-ir
+set for_what=%2
+set where=%2
 
-grep %flags% "%1" . %2 | sort | head -n %count%
+grep %flags% "%for_what%" "%where%" | sort | head -n %count%
+
+grep %flags% "%for_what%" "%where%" | sort | head -n %count%
 
 REM USAGE: grep what where
 REM EXAMPLE: grep CHUBBY C:\Documents\.BuildServer\system\artifacts\Plinkton
