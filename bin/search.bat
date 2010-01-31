c8aa920f91e1ba6f3b922a493f85c6a9eedaebd1
Ben Biddington <ben.biddington@gmail.com>
2010-01-31 21:43:29 +0000

    Changes to correct (hard-wired) directory before grepping

--- bin/search.bat
+++ bin/search.bat
@@ -2,10 +2,16 @@
 
 set count=100
 set flags=-ir
-set for_what=%2
+set for_what=%1
 set where=%2
 
-grep %flags% "%for_what%" "%where%" | sort | head -n %count%
+set base_dir="C:\TeamCity\webapps\ROOT\plugins\coriander-haarlem\bin"
+
+cd %base_dir%
+
+grep.exe %flags% %for_what% "%where%" | sort | head -n %count%
+
+exit 0
 
 REM USAGE: grep what where
 REM EXAMPLE: grep CHUBBY C:\Documents\.BuildServer\system\artifacts\Plinkton
