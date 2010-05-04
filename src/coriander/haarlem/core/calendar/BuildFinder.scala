79e6f3f949e94ba639525a020d9523ddd801e50d
Ben Biddington <ben.biddington@gmail.com>
2010-05-04 13:57:22 +0100

    Describing returns values and interaction with build server

--- src/coriander/haarlem/core/calendar/BuildFinder.scala
+++ src/coriander/haarlem/core/calendar/BuildFinder.scala
@@ -1,9 +1,16 @@
 package coriander.haarlem.core.calendar
 
-import jetbrains.buildServer.serverSide.ProjectManager
+import jetbrains.buildServer.serverSide.{SFinishedBuild, SBuildType, ProjectManager}
+import collection.mutable.ListBuffer
+import coriander.haarlem.core.Convert
 
 class BuildFinder(val projectManager : ProjectManager) {
-	def find() {
-		val allBuildTypes = projectManager.getAllBuildTypes;	
+	def find() : List[SFinishedBuild] = allSuccessfulBuilds;
+
+	private def allSuccessfulBuilds : List[SFinishedBuild] = {
+		val allBuildTypes = Convert.toScalaList(projectManager.getAllBuildTypes);
+
+		allBuildTypes.
+			map(_.getLastSuccessfullyFinished)
 	}
 }
--- test/coriander/haarlem/unit/tests/core/calendar/BuildFinderTests.scala
+++ test/coriander/haarlem/unit/tests/core/calendar/BuildFinderTests.scala
@@ -1,25 +1,34 @@
 package coriander.haarlem.unit.tests.core.calendar
 
-import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
 import coriander.haarlem.core.calendar.BuildFinder
 import org.mockito.Mockito._
 import org.mockito.Matchers._
 import coriander.haarlem.core.Convert
-import jetbrains.buildServer.serverSide.{SBuildType, SFinishedBuild, ProjectManager, SBuildServer}
+import jetbrains.buildServer.serverSide.{SBuildType, SFinishedBuild, ProjectManager}
+import org.scalatest.{BeforeAndAfterEach, Spec}
 
-class BuildFinderTests extends Spec with ShouldMatchers {
-	def beforeEach() {
-		projectManager = null
+class BuildFinderTests extends Spec with ShouldMatchers with BeforeAndAfterEach {
+	override def beforeEach() {
+		given_a_single_build_type
+		given_a_finder
+		result = null
 	}
 
 	describe("BuildFinder") {
 		it("Queries the build server") {
-			given_a_project_manager
-			given_a_finder
-			
-			when_finder_is_asked_to_find_sommit
+			given_no_finished_builds
+
+			when_asked_to_find_sommit
+
 			then_it_queries_the_project_manager_for_all_build_types
+		}
+
+		it("Collects all successful builds") {
+			given_one_finished_build
+			when_asked_to_find_sommit
+
+			result.length should equal(1)
 		}
 
 		it("Returns only builds completed within date range") (pending)
@@ -29,24 +38,32 @@ class BuildFinderTests extends Spec with ShouldMatchers {
 		it("Ignores builds that are currently running") (pending)
 	}
 
+	private def given_a_single_build_type {
+		buildType = mock(classOf[SBuildType])
+		projectManager = mock(classOf[ProjectManager])
+
+		when(projectManager.getAllBuildTypes).thenReturn(Convert.toJavaList(List(buildType)))
+	}
+
 	private def given_a_finder {
 		finder = new BuildFinder(projectManager)	
 	}
 
-	private def given_there_are_no_builds {
-		// @todo: implement	
+	private def given_no_finished_builds {
+		require(buildType != null, "No build type")
+		when(buildType.getLastChangesSuccessfullyFinished).thenReturn(null)
 	}
 
-	private def given_a_project_manager {
-		val emptyList = Convert.toJavaList[SBuildType](List())
-		projectManager = mock(classOf[ProjectManager])
-		when(projectManager.getAllBuildTypes).thenReturn(emptyList)
+	private def given_one_finished_build {
+		val fakeFinishedBuild : SFinishedBuild = mock(classOf[SFinishedBuild])
+		when(fakeFinishedBuild.getBuildDescription).thenReturn("A fake example build")
+		when(buildType.getLastChangesSuccessfullyFinished).thenReturn(fakeFinishedBuild)
 	}
 
-	private def when_finder_is_asked_to_find_sommit {
+	private def when_asked_to_find_sommit {
 		require(finder != null, "Can't proceed without the CUT (there is no finder).")
 
-		finder.find()
+		result = finder.find()
 	}
 
 	private def then_it_queries_the_project_manager_for_all_build_types {
@@ -55,4 +72,6 @@ class BuildFinderTests extends Spec with ShouldMatchers {
 
 	private var projectManager 	: ProjectManager = null
 	private var finder 			: BuildFinder = null
+	private var result			: List[SFinishedBuild] = null
+	private var buildType		: SBuildType = null
 }
