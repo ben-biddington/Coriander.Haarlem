c00cc1bfa1d5bf1da826cdd1e2a3e9cbd427f021
Ben Biddington <ben.biddington@gmail.com>
2010-05-06 13:30:15 +0100

    Reorganising terms, clearing up the intent.

--- test/coriander/haarlem/unit/tests/core/calendar/BuildFinderTests.scala
+++ test/coriander/haarlem/unit/tests/core/calendar/BuildFinderTests.scala
@@ -1,37 +1,41 @@
 package coriander.haarlem.unit.tests.core.calendar
 
-import org.scalatest.matchers.ShouldMatchers
+import org.joda.time.Instant
+import collection.mutable.ListBuffer
 import coriander.haarlem.core.calendar.BuildFinder
-import org.mockito.Mockito._
-import org.mockito.Matchers._
 import coriander.haarlem.core.Convert
 import jetbrains.buildServer.serverSide.{SBuildType, SFinishedBuild, ProjectManager}
+import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.{BeforeAndAfterEach, Spec}
-import org.joda.time.Instant
+import org.mockito.Mockito._
+import org.mockito.Matchers._
 
 class BuildFinderTests extends Spec with ShouldMatchers with BeforeAndAfterEach {
 	override def beforeEach() {
-		given_a_single_build_type
-		given_a_finder
 		result = null
 	}
 
 	describe("BuildFinder") {
-		it("Queries the build server") {
+		it("Queries the project manager for all build types") {
 			given_no_finished_builds
 
-			when_asked_to_find_sommit
+			given_a_finder
+
+			when_it_is_asked_to_find_sommit
 
 			then_it_queries_the_project_manager_for_all_build_types
-
 			then_the_result_is_empty
 		}
 
-		it("Collects all successful builds") {
-			given_one_finished_build
-			when_asked_to_find_sommit
+		it("Collects all successfully finished build instances from all build types") {
+			given_a_build_type_with_a_finished_build
+			given_a_build_type_with_a_finished_build
+			
+			given_a_finder
 
-			result.length should equal(1)
+			when_it_is_asked_to_find_sommit
+
+			result.length should equal(2)
 		}
 
 		it("Returns only builds completed within date range") (pending)
@@ -41,28 +45,38 @@ class BuildFinderTests extends Spec with ShouldMatchers with BeforeAndAfterEach 
 		it("Ignores builds that are currently running") (pending)
 	}
 
-	private def given_a_single_build_type {
-		buildType = mock(classOf[SBuildType])
+	private def given_a_finder {
+		given_a_project_manager
+		finder = new BuildFinder(projectManager)
+	}
+	
+	private def given_a_project_manager {
 		projectManager = mock(classOf[ProjectManager])
 
-		when(projectManager.getAllBuildTypes).thenReturn(Convert.toJavaList(List(buildType)))
-	}
-
-	private def given_a_finder {
-		finder = new BuildFinder(projectManager)	
+		when(projectManager.getAllBuildTypes).
+		thenReturn(Convert.toJavaList(buildTypes.toList))
 	}
 
 	private def given_no_finished_builds {
-		require(buildType != null, "No build type")
-		when(buildType.getLastSuccessfullyFinished).thenReturn(null)
+		val newBuildType = newFakeBuildType
+
+		when(newBuildType.getLastSuccessfullyFinished).
+		thenReturn(null)
+
+		buildTypes += newBuildType
 	}
 
-	private def given_one_finished_build {
-		val fake = newFakeFinishedBuild
-		when(buildType.getLastSuccessfullyFinished).thenReturn(fake)
+	private def given_a_build_type_with_a_finished_build {
+		val newBuildType = newFakeBuildType
+		val fakeFinishedBuild = newFakeFinishedBuild
+
+		when(newBuildType.getLastSuccessfullyFinished).
+		thenReturn(fakeFinishedBuild)
+
+		buildTypes += newBuildType
 	}
 
-	private def when_asked_to_find_sommit {
+	private def when_it_is_asked_to_find_sommit {
 		require(finder != null, "Can't proceed without the CUT (there is no finder).")
 
 		result = finder.find()
@@ -83,9 +97,10 @@ class BuildFinderTests extends Spec with ShouldMatchers with BeforeAndAfterEach 
 		fakeFinishedBuild
 	}
 
+	private def newFakeBuildType =  mock(classOf[SBuildType])
+
 	private var projectManager 	: ProjectManager = null
 	private var finder 			: BuildFinder = null
 	private var result			: List[SFinishedBuild] = null
-	private var buildType		: SBuildType = null
-	private var buildId			: Long = 0L
+	private var buildTypes		: ListBuffer[SBuildType] = new ListBuffer[SBuildType]
 }
