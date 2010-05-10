84204691b3b80d6ff286bf0fcc0f18e455ca601e
Ben Biddington <ben.biddington@gmail.com>
2010-05-10 13:56:21 +0100

    Renaming: we're not excluding failed builds anymore

--- src/coriander/haarlem/core/calendar/BuildFinder.scala
+++ src/coriander/haarlem/core/calendar/BuildFinder.scala
@@ -9,26 +9,27 @@ class BuildFinder(val projectManager : ProjectManager) {
 	def find() : List[SFinishedBuild] = find(FilterOptions.ALL);
 	
 	def find(options : FilterOptions) : List[SFinishedBuild] = {
-		if (options.interval != null) allBuildsIn(options.interval) else allSuccessfulBuilds
+		if (options.interval != null) allBuildsIn(options.interval) else allBuilds
 	}
 
 	private def allBuildsIn(interval : Interval) : List[SFinishedBuild] = {
-		allSuccessfulBuilds.filter((build : SFinishedBuild) => {
-			val finishedAtUTC = new DateTime(build.getFinishDate, DateTimeZone.UTC)
+		allBuilds.
+			filter((build : SFinishedBuild) => {
+				val finishedAtUTC = new DateTime(build.getFinishDate, DateTimeZone.UTC)
 			
-			interval contains(finishedAtUTC)
-		})
+				interval contains(finishedAtUTC)
+			}).
+			sort((left, right) =>
+				new DateTime(left.getFinishDate).isAfter(new DateTime(right.getFinishDate))
+			)
 	}
 
-	private def allSuccessfulBuilds : List[SFinishedBuild] = {
+	private def allBuilds : List[SFinishedBuild] = {
 		val allBuildTypes = Convert.toScalaList(projectManager.getAllBuildTypes);
 		
 		allBuildTypes.map(buildType => 
 			fullHistory(buildType)).
-			flatten[SFinishedBuild].
-			sort((left, right) =>
-				new DateTime(left.getFinishDate).isAfter(new DateTime(right.getFinishDate))
-			)
+			flatten[SFinishedBuild]
 	}
 
 	private def fullHistory(_of : SBuildType) =
