65fecf709d2c80b27336fc11dd48020fd239d5d4
Ben Biddington <ben.biddington@gmail.com>
2010-05-11 22:38:10 +0100

    Added IntervalParser

---
+++ src/coriander/haarlem/core/calendar/IntervalParser.scala
@@ -0,0 +1,9 @@
+package coriander.haarlem.core.calendar
+
+import org.joda.time.Interval
+
+class IntervalParser {
+	def parse(what : String) : Interval = {
+		throw new Exception("Not yet implemented")
+	}
+}
---
+++ test/coriander/haarlem/unit/tests/core/calendar/IntervalParserTests.scala
@@ -0,0 +1,11 @@
+package coriander.haarlem.unit.tests.core.calendar
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ben
+ * Date: 11-May-2010
+ * Time: 22:08:54
+ * To change this template use File | Settings | File Templates.
+ */
+
+class IntervalParserTests
