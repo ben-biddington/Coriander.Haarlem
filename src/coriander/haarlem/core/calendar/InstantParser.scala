671a0041539355c57b70bf861b82c9a9f49365d1
Ben Biddington <ben.biddington@gmail.com>
2010-05-12 13:10:27 +0100

    Can parse days and weeks

--- src/coriander/haarlem/core/calendar/InstantParser.scala
+++ src/coriander/haarlem/core/calendar/InstantParser.scala
@@ -2,28 +2,29 @@ package coriander.haarlem.core.calendar
 
 import java.lang.Integer._
 import util.matching.Regex
-import util.matching.Regex.Match
 import org.joda.time.{Instant}
 import org.joda.time.Days._
 
 class InstantParser(val now : Instant) {
 	def parse(what : String) : Instant = {
-		val pattern : Regex = new Regex("([\\d]+)-(day[s]?)-ago", "count", "unit")
+		val pattern : Regex = new Regex("([\\d]+)-(day|week)s?-ago", "count", "unit")
 
-		val theMatch : Option[Match] = pattern.findFirstMatchIn(what)
+		val theMatch = pattern.findFirstMatchIn(what)
 
 		if (theMatch == None)
-			throw new Exception("The value did not match expected pattern")
+			throw new Exception(
+				"Parse failed. " +
+				"The value <" + what + "> did not match expected pattern."
+			)
 
 		val count = parseInt(theMatch.get.group("count"))
-		val unit = theMatch.get.group("unit")
+		val unit = theMatch.get.group("unit").toString.toLowerCase
 
-		val result = daysAgo(count)
-
-		println(result)
-
-		result
+		if (unit == "day") daysAgo(count) else weeksAgo(count)
 	}
+
+	private def weeksAgo(howMany : Int) =
+		daysAgo(howMany * 7)
 
 	private def daysAgo(howMany : Int) =
 		now.minus(days(howMany).toStandardDuration)
--- test/coriander/haarlem/unit/tests/core/calendar/InstantParserTests.scala
+++ test/coriander/haarlem/unit/tests/core/calendar/InstantParserTests.scala
@@ -25,12 +25,30 @@ class InstantParserTests extends Spec
 			then_result_is_more_or_less(sevenDaysAgo)
 		}
 
-		it("supports optional plurals") {
+		it("you can use \"day\" or \"days\", it's up to you") {
 			when_parsing("1-day-ago")
 			then_result_is_more_or_less(yesterday)
 
 			when_parsing("1-days-ago")
 			then_result_is_more_or_less(yesterday)
+		}
+	}
+
+	describe("Parsing weeks ago") {
+		it("requires pattern dd-week(s)-ago") {
+			when_parsing("1-week-ago")
+			then_result_is_more_or_less(sevenDaysAgo)
+
+			when_parsing("2-weeks-ago")
+			then_result_is_more_or_less(fourteenDaysAgo)
+		}
+
+		it("you can use \"week\" or \"weeks\", it's up to you") {
+			when_parsing("1-week-ago")
+			then_result_is_more_or_less(sevenDaysAgo)
+
+			when_parsing("1-weeks-ago")
+			then_result_is_more_or_less(sevenDaysAgo)
 		}
 	}
 
@@ -56,5 +74,6 @@ class InstantParserTests extends Spec
 
 	private val yesterday 			= now minus(days(1).toStandardDuration)
 	private val sevenDaysAgo 		= now minus(days(7).toStandardDuration)
+	private val fourteenDaysAgo 	= now minus(days(14).toStandardDuration)
 	private val sinceYesterday 		= new Interval(yesterday, now)
 }
