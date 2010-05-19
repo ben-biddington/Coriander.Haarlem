ccad861fa203b26d82d6466dcd45832a1ff5aa6d
Ben Biddington <ben.biddington@gmail.com>
2010-05-19 09:45:49 +0100

    Refactor: tidy

--- src/coriander/haarlem/core/http/query/Query.scala
+++ src/coriander/haarlem/core/http/query/Query.scala
@@ -1,16 +1,11 @@
 package coriander.haarlem.http.query
 
 class Query(queryString : String) {
-	val namesAndValues = new QueryParser().parse(queryString)	
+	private val namesAndValues = new QueryParser().parse(queryString)	
 
-	def value(name : String) :  String =
-		if (contains(name)) get(name) else null
-
-	def containsWithValue(name : String) =
-		contains(name) && value(name) != null
-
-	def contains(name : String) =
-		namesAndValues.exists(_.getName == name)
+	def value(name : String) 				= if (contains(name)) get(name) else null
+	def containsWithValue(name : String) 	= contains(name) && value(name) != null
+	def contains(name : String) 			= namesAndValues.exists(_.getName == name)
 
 	private def get(what : String) =
 		namesAndValues.find(_.getName == what).get.getValue
