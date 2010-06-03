d62d5348a7b56d32e0813b49741deb9ade901c09
Ben Biddington <ben.biddington@gmail.com>
2010-06-03 21:04:54 +0100

    "\.BuildServer\plugins" lives in HOMEDIR on windows

--- build/local.deploy.bat
+++ build/local.deploy.bat
@@ -1,7 +1,7 @@
 @echo off
 set plugin_name=coriander-haarlem
 
-set plugin_root="C:\Documents\.BuildServer\plugins"
+set plugin_root="%HOMEPATH%\.BuildServer\plugins"
 
 echo Copying "%1" to "%plugin_root%"
 copy /Y "%1" "%plugin_root%"
