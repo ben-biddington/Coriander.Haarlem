2d4d60a90a0c6654cb874365f28852aae1909f74
Ben Biddington <ben.biddington@gmail.com>
2010-06-04 15:43:49 +0100

    Local deploy uses "TEAMCITY_BUILDSERVER_ROOT" environment variable

--- build/local.deploy.bat
+++ build/local.deploy.bat
@@ -1,7 +1,9 @@
 @echo off
 set plugin_name=coriander-haarlem
 
-set plugin_root="%HOMEPATH%\.BuildServer\plugins"
+rem note: set permanently using: setx TEAMCITY_BUILDSERVER_ROOT "C:\Documents\.BuildServer"
+
+set plugin_root="%TEAMCITY_BUILDSERVER_ROOT%\plugins"
 
 echo Copying "%1" to "%plugin_root%"
 copy /Y "%1" "%plugin_root%"
